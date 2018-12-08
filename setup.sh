1852643df66561bde0be57b2bfd77524e50198f2
Thomas Lane <thomas_lane@icloud.com>
2018-12-08 11:12:47 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 apk update
 apk upgrade
 
-apk add vi git openssh make python2 python3 lynx zsh bash vim nano mpd mpc mpv netcat-openbsd ncurses tmux neofetch curl w3m weechat wget cfdisk util-linux rover bash-completion busybox-extras sudo
+apk add git openssh make python2 python3 lynx zsh bash vim nano mpd mpc mpv netcat-openbsd ncurses tmux neofetch curl w3m weechat wget cfdisk util-linux rover bash-completion busybox-extras sudo
 
 nano /etc/passwd
 
