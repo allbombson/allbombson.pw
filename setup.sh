8418c94f68bbd28d638266e08c5c7827678cc27e
Thomas Lane <thomas_lane@icloud.com>
2018-12-08 10:23:36 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 apk update
 apk upgrade
 
-apk add git openssh make python2 python3 lynx zsh bash vim nano mpd mpc mpv netcat-openbsd ncurses tmux neofetch curl w3m weechat wget cfdisk util-linux rover 
+apk add git openssh make python2 python3 lynx zsh bash vim nano mpd mpc mpv netcat-openbsd ncurses tmux neofetch curl w3m weechat wget cfdisk util-linux rover shadow bash-completion
 
+
+mkdir ~/.scripts
 curl -LO https://raw.githubusercontent.com/allbombson/alpine-dotfiles/master/.profile
 curl -LO https://raw.githubusercontent.com/allbombson/alpine-dotfiles/master/.bashrc
 
