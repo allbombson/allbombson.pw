c3f79550d3cf2d88ba91b81522d44db2bf6445dd
Thomas Lane <thomas_lane@icloud.com>
2018-12-07 20:48:00 -0800

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,4 @@
+apk add git openssh make python2 python3 lynx zsh bash vim nano mpd mpc mpv netcat-openbsd ncurses tmux neofetch curl w3m weechat wget
+
+curl -LO https://raw.githubusercontent.com/allbombson/dotfiles/master/.profile
+curl -LO https://raw.githubusercontent.com/LukeSmithxyz/voidrice/master/.bashrc
