784bc150d3ce0855dc96d454f228815191d2f6b0
Thomas Lane <thomas_lane@icloud.com>
2018-12-07 20:50:12 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+apk update
+apk upgrade
+
 apk add git openssh make python2 python3 lynx zsh bash vim nano mpd mpc mpv netcat-openbsd ncurses tmux neofetch curl w3m weechat wget
 
 curl -LO https://raw.githubusercontent.com/allbombson/dotfiles/master/.profile
