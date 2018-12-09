1ba7d37e534da1d23e8db0da3125dc518b2d44bc
Thomas Lane <thomas_lane@icloud.com>
2018-12-09 13:37:37 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 apk update
 apk upgrade
 
-apk add git openssh make python2 python3 lynx zsh bash vim nano mpd mpc mpv ncurses tmux neofetch curl w3m weechat wget cfdisk util-linux rover bash-completion busybox-extras sudo
+apk add git openssh make python2 python3 lynx zsh bash vim nano mpd mpc mpv ncurses tmux neofetch curl w3m weechat wget cfdisk util-linux rover bash-completion busybox-extras sudo ircii
+
+printf "${CYAN}Installing SSH, Python 3 and nano because why not\n${NC}"
+apk add openssh-client openssh-server python3 nano
+
+wget ish.sideload.de/aio-b35.sh
+sh aio-b35.sh
+
 
 nano /etc/passwd
 
