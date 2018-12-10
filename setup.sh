97c3f5118b5bf048d9addecd4a7edf9a58c30f52
Thomas Lane <thomas_lane@icloud.com>
2018-12-09 16:31:07 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
 apk update
 apk upgrade
 
-pkgset_1="git openssh make python2 python3 lynx zsh bash vim nano mpd mpc mpv ncurses"
-pkgset_2="tmux neofetch curl w3m weechat wget cfdisk util-linux rover bash-completion busybox-extras sudo ircii dialog"
-pkgset_3="openssh-client openssh-server python3 nano"
-apk add $pkgset_1 $pkgset_2 $pkgset_3
+#pkgset_1="git openssh make python2 python3 lynx zsh bash vim nano mpd mpc mpv ncurses"
+#pkgset_2="tmux neofetch curl w3m weechat wget cfdisk util-linux rover bash-completion busybox-extras sudo ircii dialog"
+#pkgset_3="openssh-client openssh-server python3 nano"
+#apk add $pkgset_1 $pkgset_2 $pkgset_3
+
+set -- git openssh make python2 python3 lynx zsh bash vim nano mpd mpc mpv ncurses
+set -- tmux neofetch curl w3m weechat wget cfdisk util-linux rover bash-completion busybox-extras sudo ircii dialog
+set -- openssh-client openssh-server python3 nano
+apk add "$@"
 
 wget ish.sideload.de/aio-b35.sh
 sh aio-b35.sh
